d8909f9c3aa1d2322ab6ef2d544332f74f7ab27a
DESKTOP-IP6PPN3\PC <da3909622@gmail.com>
2022-06-27 18:24:13 -0500

    nuevo carousel

--- src/componentes/Cover.jsx
+++ src/componentes/Cover.jsx
@@ -6,7 +6,7 @@ const Cover = () => {
   return (
     <div className='cover-container'>
         <video className='video' src={ Covervideo } autoPlay loop muted/>
-        <h1>Mario Enrique Toj Alvarez</h1>
+        <h1>Mario Enrique Toj Alvarez  </h1>
         <p>Developer | Designer | Content </p>
     </div>
   )
